4553dcf64331a21bf91e13c06942552a0a7403ff
heiye1991 <1604043458@qq.com>
2021-11-01 10:39:15 +0800

    路由系统搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styles from './App.module.css'
-import { HomePage } from './pages'
+import { HomePage, SignInPage, RegisterPage, DetailPage } from './pages'
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
         <Switch>
           {/* exact 当路由一样的时候才会加载。使用 Switch组件解决页面叠加的问题，每次路由只匹配一条路径而且是优先级最高的路径来渲染。404页面放在最后面。 */}
           <Route exact path='/' component={HomePage} />
-          <Route path='/signIn' render={() => <h1>登录页面</h1>} />
-          <Route path='/sign' render={() => <h1>注册页面</h1>} />
+          <Route path='/signIn' component={SignInPage} />
+          <Route path='/register' component={RegisterPage} />
+          <Route path='/detail/:touristRouteId' component={DetailPage} />
           <Route render={() => <h1>404 not found</h1>} />
         </Switch>
       </Router>
