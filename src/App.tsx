9784fe94d5ed58c766cedbecf9cfba5e5d80b702
heiye1991 <1604043458@qq.com>
2021-11-01 10:03:04 +0800

    引入react-router-dom路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import styles from './App.module.css'
 import { HomePage } from './pages'
 
 function App() {
   return (
     <div className={styles.App}>
-      <HomePage />
+      <Router>
+        <Route path='/' component={HomePage} />
+      </Router>
     </div>
   )
 }
