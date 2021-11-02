d80ab4e2e9aabb1faa112bf0891368f45ea11300
heiye1991 <1604043458@qq.com>
2021-11-02 23:00:33 +0800

    使用axios获取携程旅游数据

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,16 +1,108 @@
 import React from 'react'
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Typography, Spin } from 'antd'
 import { withTranslation, WithTranslation } from 'react-i18next'
+import axios from 'axios'
 import { Header, Footer, SideMenu, Carousel, ProductCollection, BusinessPartners } from '../../components/index'
-import { productList1, productList2, productList3 } from './mockups'
 import sideImage1 from '../../assets/images/sider_2019_12-09.png'
 import sideImage2 from '../../assets/images/sider_2019_02-04.png'
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png'
 import styles from './HomePage.module.css'
 
-class HomePageComponent extends React.Component<WithTranslation> {
+interface State {
+  loading: boolean
+  error: string | null
+  productList1: any[]
+  productList2: any[]
+  productList3: any[]
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+      error: null,
+      productList1: [],
+      productList2: [],
+      productList3: [],
+    }
+  }
+  componentDidMount() {
+    // 获取每个旅游栏目的数据列表
+    function getProducts(catgoryID: number) {
+      return axios.post('https://vacations.ctrip.com/restapi/gateway/14422/displayWindow', {
+        catgoryID,
+        channel: 'Online',
+        siteID: 1,
+        startCity: 1,
+        version: 'B',
+      })
+    }
+    // 处理获取到的数据列表
+    function dealProducts(arr: any[]) {
+      const products: any[] = []
+      for (const item of arr) {
+        products.push({
+          id: item.id,
+          price: item.price,
+          title: item.name,
+          touristRoutePictures: [
+            {
+              url: item.img,
+            },
+          ],
+        })
+      }
+      return products
+    }
+    axios
+      .all([getProducts(1), getProducts(2), getProducts(3)])
+      .then(
+        axios.spread((res1, res2, res3) => {
+          const arr1 = res1.data.displayWindowModels[0].tabList[0].products
+          const products1 = dealProducts(arr1)
+
+          const arr2 = res2.data.displayWindowModels[0].tabList[0].products
+          const products2 = dealProducts(arr2)
+
+          const arr3 = res1.data.displayWindowModels[0].tabList[0].products
+          const products3 = dealProducts(arr3)
+
+          this.setState({
+            loading: false,
+            error: null,
+            productList1: products1,
+            productList2: products2,
+            productList3: products3,
+          })
+        }),
+      )
+      .catch(err => {
+        this.setState({
+          loading: false,
+          error: err.message,
+        })
+      })
+  }
   render() {
     const { t } = this.props
+    const { productList1, productList2, productList3, loading, error } = this.state
+
+    if (loading) {
+      return (
+        <Spin
+          size='large'
+          style={{
+            margin: '50vh auto',
+            width: '100%',
+          }}
+        />
+      )
+    }
+    if (error) {
+      return <div>网站出错：{error}</div>
+    }
+
     return (
       <>
         <Header />
