9f07d2de7f546ee48ca08458e575f2b0d8ed4a16
heiye1991 <1604043458@qq.com>
2021-11-09 11:05:42 +0800

    修复使用redux-persist导致的action type错误

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import languageReducer from './language/languageReducer'
 import recommendProductsReducer from './recommendProducts/recommendProductsReducer'
@@ -26,7 +26,14 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware => [...getDefaultMiddleware()].concat(actionLog),
+  middleware: getDefaultMiddleware =>
+    [
+      ...getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+    ].concat(actionLog),
   devTools: true,
 })
 
